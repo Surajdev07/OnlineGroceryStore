5e731089fcd1fed25e05046bbbe262ed36eca047
shubham <SHUBHAM.BHATNAGAR@CAPGEMINI.COM>
2021-12-06 16:02:54 +0530

    6-12-upload

--- Online Grocery Shop/onlinegroceryshopapi/src/main/java/com/groceryshopapi/web/GroceryContoller.java
+++ Online Grocery Shop/onlinegroceryshopapi/src/main/java/com/groceryshopapi/web/GroceryContoller.java
@@ -16,8 +16,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.groceryshopapi.domain.Category;
 import com.groceryshopapi.domain.Grocery;
 import com.groceryshopapi.exception.GroceryIDException;
 import com.groceryshopapi.service.GroceryService;
@@ -40,16 +42,47 @@ public class GroceryContoller {
 		return groceryService.getGrocery();
 	}
 
+//	@CrossOrigin("http://localhost:4200")
+//	@PostMapping("/add")
+//	public ResponseEntity<?> createGrocery(@Valid @RequestBody Grocery grocery, BindingResult result){
+//		
+//		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(result);
+//		if(errorMap!=null) return errorMap;
+//		
+//		Grocery grocery2 = groceryService.save(grocery);
+//		return new ResponseEntity<Grocery>(grocery2, HttpStatus.CREATED);
+//	}
+	
+	
 	@CrossOrigin("http://localhost:4200")
 	@PostMapping("/add")
-	public ResponseEntity<?> createGrocery(@Valid @RequestBody Grocery grocery, BindingResult result){
+	public String addgrocery(@RequestParam String id) {
+		System.out.println(id); 
+		String categoryIdentifier1=id.split("-")[0];
+		String groceryidentifier=id.split("-")[1];
+		String groceryName=id.split("-")[2];
+		String imageLink1=id.split("-")[3];
+		String netPrice=id.split("-")[4];
+		String offerIdentifier=id.split("-")[5];
+		String price=id.split("-")[6];
+		String quantityAvailable=id.split("-")[7];
+		System.out.println(categoryIdentifier1+":"+groceryidentifier+":"+groceryName+":");
+//		String routerLink=id.split("-")[3];
+		System.out.println("success");
+		Grocery grocery = new Grocery();
+		grocery.setCategoryIdentifier(categoryIdentifier1);
+		grocery.setGroceryIdentifier(groceryidentifier);
+		grocery.setGroceryName(groceryName);
+		grocery.setImageLink(imageLink1);
+		grocery.setNetPrice(0);
+		grocery.setOfferIdentifier(offerIdentifier);
+		grocery.setPrice(0);
+		grocery.setQuantityAvailable(0);
 		
-		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(result);
-		if(errorMap!=null) return errorMap;
-		
-		Grocery grocery2 = groceryService.save(grocery);
-		return new ResponseEntity<Grocery>(grocery2, HttpStatus.CREATED);
-	}
+		groceryService.save(grocery);
+		System.out.println("success");
+		return id;
+		}
 
 //	@CrossOrigin("http://localhost:4200")
 //	@GetMapping("/{categoryIdentifier}")
